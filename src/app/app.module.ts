295d85c2c7549cf7259ccd57f3b20a38accd40a1
rehemafaith <rehemafaith01@gmail.com>
2019-03-22 17:07:11 +0300

    add initial page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+
+
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    
+    
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
